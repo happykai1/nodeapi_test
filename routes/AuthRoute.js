82802608ec06e4dff52a6880062aaec0df7c351b
Natkitta Apiromkasem <watsak1@hotmail.com>
2023-02-01 10:26:06 +0700

    Update AuthRoute.js

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const authServices = require('../service/AuthService')
+
+const authRouter = express.Router();
+
+authRouter.post('/api/login',authServices.createJWT)
+authRouter.get('/api/profile',authServices.verifyToken)
+
+module.exports = authRouter;
 
